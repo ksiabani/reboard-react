825cb8686f5286208e24f91a5c6bfecb432fb767
Kostas Siabanis <k.siabanis@exus.co.uk>
2017-10-31 13:20:29 +0200

    Continue folder restructuring and clean up files

--- src/modules/redux/reducers/index.js
+++ src/modules/redux/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-// import counter from './counterReducer';
-// import drawerStatus from './wrapperReducer';
 
 const rootReducer =  combineReducers({
     routing: routerReducer
-    // drawerStatus
-    // counter
 });
 
 export default rootReducer;
