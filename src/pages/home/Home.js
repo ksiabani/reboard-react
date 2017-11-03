72e7c8704e39935faf4d975eab86e056fcebf646
Kostas Siabanis <kostas.siabanis@gmail.com>
2017-11-03 11:20:51 +0200

    Stats cards now fed from stats array

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {withStyles} from 'material-ui/styles';
 import Grid from 'material-ui/Grid';
 import Card, {CardActions, CardContent} from 'material-ui/Card';
-import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
@@ -12,7 +11,7 @@ const styles = theme => ({
     card: {
         // minWidth: 275,
         backgroundColor: '#fe7675'
-     },
+    },
     bullet: {
         display: 'inline-block',
         margin: '0 2px',
@@ -40,84 +39,63 @@ const styles = theme => ({
     }
 });
 
+const stats = [
+    {
+        id: 1,
+        value: '10,152',
+        text: 'stargazers',
+        icon: 'star',
+        color: '#fe7675'
+    },
+    {
+        id: 2,
+        value: '2,344',
+        text: 'forks',
+        icon: 'content_copy',
+        color: '#2dabe5'
+    },
+    {
+        id: 3,
+        value: '689',
+        text: 'open issues',
+        icon: 'bug_report',
+        color: '#ffc36d'
+    },
+    {
+        id: 4,
+        value: '1,000',
+        text: 'subscribers',
+        icon: 'rss_feed',
+        color: '#54e69d'
+    }
+];
+
 const Home = props => {
     const {classes} = props;
-    const bull = <span className={classes.bullet}>•</span>;
+    // const bull = <span className={classes.bullet}>•</span>;
 
     return (
         <Grid container>
 
-            <Grid item xs={12} md={6} lg={3}>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Typography type="headline" component="h2" className={classes.headline}>
-                            10.152
-                        </Typography>
-                        <Typography type="body1" className={classes.pos}>
-                            stargazers
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardActions}>
-                        <IconButton color="contrast">
-                            <Icon>more_vert</Icon>
-                        </IconButton>
-                    </CardActions>
-                </Card>
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={3}>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Typography type="headline" component="h2" className={classes.headline}>
-                            2.344
-                        </Typography>
-                        <Typography type="body1" className={classes.pos}>
-                            forks
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardActions}>
-                        <IconButton color="contrast">
-                            <Icon>more_vert</Icon>
-                        </IconButton>
-                    </CardActions>
-                </Card>
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={3}>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Typography type="headline" component="h2" className={classes.headline}>
-                            689
-                        </Typography>
-                        <Typography type="body1" className={classes.pos}>
-                            open issues
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardActions}>
-                        <IconButton color="contrast">
-                            <Icon>more_vert</Icon>
-                        </IconButton>
-                    </CardActions>
-                </Card>
-            </Grid>
-
-            <Grid item xs={12} md={6} lg={3}>
-                <Card className={classes.card}>
-                    <CardContent>
-                        <Typography type="headline" component="h2" className={classes.headline}>
-                            1000
-                        </Typography>
-                        <Typography type="body1" className={classes.pos}>
-                            subscribers
-                        </Typography>
-                    </CardContent>
-                    <CardActions className={classes.cardActions}>
-                        <IconButton color="contrast">
-                            <Icon>more_vert</Icon>
-                        </IconButton>
-                    </CardActions>
-                </Card>
-            </Grid>
+            {stats.map(stat =>
+                <Grid item xs={12} md={6} lg={3} key={stat.id}>
+                    <Card className={classes.card} style={{backgroundColor: stat.color}}>
+                        <CardContent>
+                            <Typography type="headline" component="h2" className={classes.headline}>
+                                {stat.value}
+                            </Typography>
+                            <Typography type="body1" className={classes.pos}>
+                                {stat.text}
+                            </Typography>
+                        </CardContent>
+                        <CardActions className={classes.cardActions}>
+                            <IconButton color="contrast">
+                                <Icon>more_vert</Icon>
+                            </IconButton>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            )}
 
         </Grid>
     );
@@ -128,3 +106,4 @@ Home.propTypes = {
 };
 
 export default withStyles(styles)(Home);
+
